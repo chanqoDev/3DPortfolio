aa7baf7c01a3df53c49341d1b848685204073f56
Christian E <christianelena92@gmail.com>
2024-01-29 22:50:56 -0600

    adjust our plan for multiple screen size, add cursor condition to determing grab or grabbing dynamically, adjust plane for screen size and rotate the plane

--- 3DPortfolio/src/models/Bird.jsx
+++ 3DPortfolio/src/models/Bird.jsx
@@ -4,9 +4,9 @@ import birdScene from "../assets/3d/bird.glb";
 
 const Bird = () => {
     const { scene, animations } = useGLTF(birdScene); 
-  return (
-      <mesh position={[-5, 2, 1]} scale={[0.003, 0.003, 0.003]}><primitive object={ scene} /></mesh>
-  )
+    return (
+    <mesh position={[-5, 2, 1]} scale={[0.003, 0.003, 0.003]}><primitive object={ scene} /></mesh>
+    )
 }
 
 export default Bird
--- 3DPortfolio/src/pages/Home.jsx
+++ 3DPortfolio/src/pages/Home.jsx
@@ -1,6 +1,5 @@
-import React from 'react'; 
-import { Canvas } from '@react-three/fiber'; 
-import { Suspense } from 'react'; 
+import  { useState, Suspense } from 'react'; 
+import { Canvas } from '@react-three/fiber';  
 import Loader from '../components/Loader';
 import Island from '../models/Island'; 
 import Sky from '../models/Sky';
@@ -10,6 +9,9 @@ import Plane from '../models/Plane';
   {/*  <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'> POPUP </div>  */}
 
 const Home = () => {
+  const [isRotating, setIsRotating] = useState(false); 
+
+
   const adjustIslandForScreenSize = () => {
     let screenScale = null;
     let screenPosition = [0, -6.5, -43]; 
@@ -22,11 +24,26 @@ const Home = () => {
     }
     return [screenScale, screenPosition, rotation]; 
   }
+   const adjustPlaneForScreenSize = () => {
+    let screenScale, screenPosition; 
+
+    if (window.innerWidth < 768) {
+      screenScale = [1.5, 1.5, 1.5]; 
+      screenPosition = [0, -1.5, 0]; 
+    } else {
+      screenScale = [3, 3, 3];
+      screenPosition = [0, -4, -4]; 
+    }
+    return [screenScale, screenPosition]; 
+   }
+  
   const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize(); 
+  // create a hook for the plane 
+  const [planeScale, planePosition] = adjustPlaneForScreenSize(); 
 
   return (
     <section className='w-full h-screen relative'>
-    <Canvas className='w-full h-screen bg-transparent' camera={{near: 0.1, far:1000 }}>
+    <Canvas className={`w-full h-screen bg-transparent ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`} camera={{near: 0.1, far:1000 }}>
         <Suspense fallback={<Loader />}>
           {/* DirecitonalLIght: sunlight Direction or how you want your scene to be percevied */}
           <directionalLight position={[1, 1, 1]} intensity={2} />
@@ -38,8 +55,8 @@ const Home = () => {
           
           <Bird />
           <Sky />
-          <Island position={ islandPosition} scale={islandScale} rotation={islandRotation} /> 
-          <Plane /> 
+          <Island position={islandPosition} scale={islandScale} rotation={islandRotation} isRotating={setIsRotating} /> 
+          <Plane planePosition={planePosition} planeScale={planeScale} rotation={[0, 20, 0]} /> 
         </Suspense>  
     </Canvas>
     </section>
